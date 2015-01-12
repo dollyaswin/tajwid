95d3dd34b5d381b783f496913dfc3fab1c1940be
dollyaswin <dolly.aswin@gmail.com>
2015-01-12 09:39:03 +0700

    Add template for index.php

--- index.php
+++ index.php
@@ -2,3 +2,15 @@
 $config = include_once 'config.php';
 $pdo = include_once 'pdo.php';
 
+// @todo retrieve category with parent_id is null
+$sql = "SELECT * FROM `category` WHERE `parent_id` IS NULL";
+$categories = [];
+foreach ($pdo->query($sql) as $row) {
+    $categories[] = $row;
+}
+
+// close database connection
+$pdo = null;
+
+// assign to template
+require 'template/index.phtml';
--- pdo.php
+++ pdo.php
@@ -3,6 +3,8 @@ try {
     $dbh = new PDO(
         'mysql:host=' . $config['db']['host'] . ';dbname=' . $config['db']['name'] , $config['db']['user'], $config['db']['pass']
     );
+
+    return $dbh;
 } catch(PDOException $e) {
     echo $e->getMessage();
 }
